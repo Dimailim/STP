4658792ff8da733fe4c0c0ef75972e0617b2c94f
Dmitry <d_nail@mail.ru>
2018-11-17 13:16:58 +0700

    Added all unitTests + fix  some problems

--- Lab4_Memory/Memory_Test/unittest1.cpp
+++ Lab4_Memory/Memory_Test/unittest1.cpp
@@ -3,27 +3,54 @@
 #include "../Memory/Memory.h"
 #include "../Memory/Memory.cpp"
 #include "../../Lab1_Fraction/Fraction/fraction.h"
-#include "../../Lab2_Complex/Complex/complex.h"
-#include "../../Lab3_Pnumber/PNumber/Pnumber.h"
+
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace Memory_Test
+namespace UnitTestMem
 {		
-	TEST_CLASS(UnitTestMemory)
+	TEST_CLASS(UnitTestMem)
 	{
 	public:
 		
-		TEST_METHOD(Construct)
+		TEST_METHOD(MemConstruct)
 		{
 			Memory<Fraction> f;
 			Assert::AreEqual(string("0/1"), f.get().toString());
 
-			Memory<Complex> c;
-			Assert::AreEqual(string("0+i*0"), c.get().toString());
+			
+		}
+		TEST_METHOD(MemWrite)
+		{
+			Memory<int> w;
+			w.write(0);
+			Assert::AreEqual(0, w.get());
+		}
 
-			Memory<Pnumber> p;
-			Assert::AreEqual(string("0"), p.get().getAstring());
+		TEST_METHOD(MemAdd)
+		{
+			Memory<int> a;
+			a.write(10);
+			a.add(20);
+			Assert::AreEqual(30, a.get());
+
+		}
+
+		TEST_METHOD(MemClear)
+		{
+			Memory<Fraction> f;
+			f.write(string("1/2"));
+			f.erase();
+			Assert::AreEqual(string("_Off"),f.getState());
+		}
+
+		TEST_METHOD(MemgetState)
+		{
+			Memory <int> m;
+			Memory <Fraction> f;
+			m.write(100);
+			Assert::AreEqual(string("_On"), m.getState());
+			Assert::AreEqual(string("_Off"), f.getState());
 		}
 
 	};
--- Lab5_Process/UnitTestProc/unittest1.cpp
+++ Lab5_Process/UnitTestProc/unittest1.cpp
@@ -3,23 +3,79 @@
 #include "../Process/Proc.h"
 #include "../Process/Proc.cpp"
 #include "../../Lab1_Fraction/Fraction/fraction.h"
+<<<<<<< HEAD
+=======
+#include "../../Lab2_Complex/Complex/complex.h"
+#include "../../Lab3_Pnumber/PNumber/Pnumber.h"
+>>>>>>> 164653ded26e22ba8d4345b20b786dd24505a921
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace UnitTestProc
+namespace Process_Test
 {		
-	TEST_CLASS(UnitTestProc)
+	TEST_CLASS(UnitTestProcess)
 	{
 	public:
 		
+<<<<<<< HEAD
 		TEST_METHOD(TestConstruct)
+=======
+		TEST_METHOD(TestConstructor)
+>>>>>>> 164653ded26e22ba8d4345b20b786dd24505a921
 		{
 			Proc<Fraction> f;
 			Assert::AreEqual(string("0/1"), f.getLeft().toString());
 			Assert::AreEqual(string("0/1"), f.getRight().toString());
 			Assert::AreEqual((int)f.None, f.getOptn());
+<<<<<<< HEAD
+		}
+		TEST_METHOD(ProcReset)
+		{
+			Proc<int> p;
+			p.setLeft(1);
+			p.setRight(0);
+			p.reset();
+			Assert::AreEqual((int)p.None, p.getOptn());
 
 		}
+		TEST_METHOD(OptnClear)
+		{
+			Proc<int> p;
+			p.setOptn(p.Mul);
+			p.OptnClear();
+			Assert::AreEqual((int)p.None, p.getOptn());
+		}
+		TEST_METHOD(OptnRun)
+		{
+			Proc<int> p;
+			p.setLeft(5);
+			p.setRight(10);
+			p.setOptn(p.Add);
+			p.OptnRun();
+			Assert::AreEqual(15, p.getLeft());
+		}
 
+		TEST_METHOD(FuncRun)
+		{
+			Proc<Fraction> f;
+			f.setRight(string("1/2"));
+			f.setOptn(f.Rev);
+			f.FuncRun();
+			Assert::AreEqual(string("2/1"), f.getRight().toString());
+		}
+=======
+
+			Proc<Complex> c;
+			Assert::AreEqual(string("0+i*0"), c.getLeft().toString());
+			Assert::AreEqual(string("0+i*0"), c.getRight().toString());
+			Assert::AreEqual((int)c.None, c.getOptn());
+
+			Proc<Pnumber> p;
+			Assert::AreEqual(string("0"), p.getLeft().getAstring());
+			Assert::AreEqual(string("0"), p.getRight().getAstring());
+			Assert::AreEqual((int)p.None, p.getOptn());
+		}
+
+>>>>>>> 164653ded26e22ba8d4345b20b786dd24505a921
 	};
 }
